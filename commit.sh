dbd4a2bd2b476cbfca8a8e18efa0b30c4933b413
ezulkosk <ezulkosk@gsd.uwaterloo.ca>
2014-05-23 15:40:24 -0400

    Changed file names

--- commit.sh
+++ commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ClaferZ3 -mtest -tall -g6 -n -1 --profiling
-#ClaferZ3 -mtest -tpositive -g6 -n -1
+ClaferSMT -mtest -tall -g6 -n -1 --profiling
+#ClaferSMT -mtest -tpositive -g6 -n -1
 if [ "$#" -ne 1 ]; then
     echo "No commit, forgot message."
     exit
